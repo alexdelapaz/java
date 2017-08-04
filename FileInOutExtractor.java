abdfc1ea90a1d4658cf1f2e7b838c747f0992445
everexpedition <30325628+everexpedition@users.noreply.github.com>
2017-08-04 18:40:20 -0400

    push

--- FileInOutExtractor.java
+++ FileInOutExtractor.java
@@ -16,38 +16,66 @@ public class FileInOutExtractor {
 						{
 
 	Scanner keyboard = new Scanner(System.in);
-
-	//Prompt for input and output file names.
-	System.out.print("Input file: ");
-	String inputFileString = keyboard.next();
-	System.out.print("Outputfile: ");
-	String outputFileString = keyboard.next();			//*next() to nextln() can cause it to read extra space.
-						
-			
-	File inputFile = new File(inputFileString);	//File class		
-	Scanner readFile = new Scanner(inputFile);	//Scanner input
-							//		Scanner readFile = new Scanner(new File(inputFileString);							
-	PrintWriter outputFile = new PrintWriter(outputFileString);	
-							//PrintWriter class
-									
-									//String inputFileString  C:\Users\Alex\Documents\GitHub\JavaLibrary\input.txt
-									//String outputFileString C:\Users\Alex\Documents\GitHub\JavaLibrary\output.txt
-									//String literal "C:\\Users\\Alex\\Documents\\GitHub\\JavaLibrary\\input.txt"
-									//String literal "C:\\Users\\Alex\\Documents\\GitHub\\JavaLibrary\\output.txt"				
-									
 	
-	readFile.close();		//close() Scanner
+	Scanner inputFile1 = new Scanner(new file("worldpop.txt"));
+	Scanner inputFile2 = new Scanner(new file("worldarea.txt"));
+
+	Printwriter outputFile = new Printwriter("world_pop_density.txt"); 
+
+	//Read nextLine() from each file
+	while(inputFile1.hasNextLine() && inputFile2.hasNextLine()){
+	String linesFile1 = inputFile1.nextLine();
+	String linesFile2 = inputFile2.nextLine();
+	}
+	
+	//Extract country and value
+	String countryName = extractCountry(linesFile1);
+	double population = extractValue(linesFile1);
+	double area = extractValue(linesFile2);
+
+	//Compute and output density
+	double density=0;
+					//Sentinel against division by 0
+		if(area!= 0 ){			
+		density=population/area;
+		}
+		outputFile.printf("%-40s%15.2f\n",country,density);
+	}
+
+
+
+	inputFile1.close();		//close() Scanner	
+	inputFile2.close();		//close() Scanner
 	outputFile.close();		//close() PrintWriter
 
 	}//End main method
 
+
+/**
+	Extracts first word from a word-number mixed file.
+	@param line a line containing the word of mixed file.
+	@return the word in a mixed file
+*/
 	public static String extractCountry(String input){
-
+	int i=0;
+	while(!Character.isDigit(input.charAt(i))){
+	i++;
+	return line.substring(0, i).trim();
 	}
+	}//End method
 
+/**
+	Extracts value from a word-number mixed file.
+	@param line a line containing the value in the mixed file.
+	@return the value in a mixed file
+*/
 	public static double extractValue(String input){
-
+	int i=0;
+	while(!CharacterisDigit(line.charAt(i))){
+	i++:
+	return Double.parseDouble(line.substring(i).trim());
 	}
+	}//End method
 
 	//Begin here
 
